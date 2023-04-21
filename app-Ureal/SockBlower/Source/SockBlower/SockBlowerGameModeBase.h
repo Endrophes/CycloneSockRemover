5b60aec2b1c01440d9f3df06f4b8321b91960983
Endrophes <Endrophes@users.noreply.github.com>
2023-04-21 16:31:55 -0400

    Uploading new assets and models

---
+++ app-Ureal/SockBlower/Source/SockBlower/SockBlowerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SockBlowerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOCKBLOWER_API ASockBlowerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
